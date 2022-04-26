6c1e25f451a38296de9496c0dcb54f29059e9252
rodrialeh01 <2823897250101@ingenieria.usac.edu.gt>
2022-04-25 23:01:57 -0600

    Agregando una plantilla inicial a mi frontend

--- Frontend/app/urls.py
+++ Frontend/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.home),
+    path('base.html', views.home),
 ]
--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -4,11 +4,4 @@ import requests
 # Create your views here.
 
 def home(request):
-    hi = {
-        'helou': 'hola'
-    }
-    hi.json() 
-    context = {
-        'title':hi
-    }
-    return render(request, 'index.html', context)
+    return render(request, template_name='base.html')
